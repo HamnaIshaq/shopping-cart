ba587b4196461f1f93e973f3c65a9d46d7a65a87
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-06-20 00:26:51 +0500

    Show header with all flowers from data.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Header from "./components/Header";
+import ProductCard from "./components/ProductCard";
+import { data } from "./data/data";
+import "./assets/reset.css";
+import "./assets/style.css";
+
 const App = () => {
-  return <div>app</div>;
+  return (
+    <>
+      <Header />
+      <div className="container products-container">
+        {data.map((product) => {
+          return (
+            <ProductCard
+              productImg={product.img}
+              productTitle={product.title}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default App;
