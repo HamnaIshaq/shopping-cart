4219d784318f0362a6c16aea50968ff97812b305
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-06-20 00:25:39 +0500

    All flowers img with title

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    title: "Orange Tulips",
+    img: "/flowers/orange-tulips-in-gray-pot.jpg",
+  },
+  {
+    title: "Pink & Red Flowers",
+    img: "/flowers/pink-and-red-flowers.jpg",
+  },
+  {
+    title: "Pink & Yellow Flowers",
+    img: "/flowers/pink-and-yellow-flowers.jpg",
+  },
+  {
+    title: "Pink Bouquet",
+    img: "/flowers/pink-bouquet.jpg",
+  },
+  {
+    title: "White & Orange Flowers",
+    img: "/flowers/white-and-orange-flowers.jpg",
+  },
+  {
+    title: "White Flowers",
+    img: "/flowers/white-flowers-in-black-vase.jpg",
+  },
+  {
+    title: "White Peony",
+    img: "/flowers/white-peony-bouquet.jpg",
+  },
+];
