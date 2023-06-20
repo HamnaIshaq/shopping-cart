94c516445f47c726cf4c52c52f6f7d45722c233c
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-06-20 22:50:22 +0500

    Show RouteSwitch component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import Header from "./components/Header";
-import ProductCard from "./components/ProductCard";
-import { data } from "./data/data";
 import "./assets/reset.css";
 import "./assets/style.css";
+import RouteSwitch from "./routes/RouteSwitch";
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <div className="container products-container">
-        {data.map((product) => {
-          return (
-            <ProductCard
-              productImg={product.img}
-              productTitle={product.title}
-            />
-          );
-        })}
-      </div>
-    </>
-  );
+  return <RouteSwitch />;
 };
 
 export default App;
