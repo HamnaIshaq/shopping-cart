4075a736da20ccab59b3838341d05707cdb51c2f
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-06-20 22:47:21 +0500

    Product data file

--- src/data/data.js
+++ src/data/data.js
@@ -1,29 +1,36 @@
 export const data = [
   {
+    id: 1,
     title: "Orange Tulips",
     img: "/flowers/orange-tulips-in-gray-pot.jpg",
   },
   {
+    id: 2,
     title: "Pink & Red Flowers",
     img: "/flowers/pink-and-red-flowers.jpg",
   },
   {
+    id: 3,
     title: "Pink & Yellow Flowers",
     img: "/flowers/pink-and-yellow-flowers.jpg",
   },
   {
+    id: 4,
     title: "Pink Bouquet",
     img: "/flowers/pink-bouquet.jpg",
   },
   {
+    id: 5,
     title: "White & Orange Flowers",
     img: "/flowers/white-and-orange-flowers.jpg",
   },
   {
+    id: 6,
     title: "White Flowers",
     img: "/flowers/white-flowers-in-black-vase.jpg",
   },
   {
+    id: 7,
     title: "White Peony",
     img: "/flowers/white-peony-bouquet.jpg",
   },
