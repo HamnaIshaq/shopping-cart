5b59fba417f6db50208e8e2781013fc17574a9e6
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-06-23 23:18:46 +0500

    Pass product price and currency to child component and re-factor cartItems update logic

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -7,37 +7,40 @@ const ProductList = ({
   totalCartItems,
   setTotalCartItems,
 }) => {
-  function onClickAddToCartBtn(productId, productImg, productTitle) {
+  function onClickAddToCartBtn(
+    productId,
+    productImg,
+    productTitle,
+    productPrice
+  ) {
     setTotalCartItems(totalCartItems + 1);
 
-    const item = {
-      id: productId,
-      title: productTitle,
-      img: productImg,
-      amount: 1,
-    };
+    const cartItemExist = cartItems.filter((item) => item.id === productId);
 
-    if (cartItems.length) {
-      const productExistsInArr = cartItems.some(
-        (item) => item.id === productId
-      );
-
-      if (productExistsInArr) {
-        const tempArr = cartItems;
-
-        tempArr.forEach((item) => {
-          if (item.id === productId) {
-            item.amount++;
+    if (cartItemExist.length) {
+      setCartItems((prevState) => {
+        return prevState.map((item) => {
+          const newItem = { ...item };
+          if (newItem.id === productId) {
+            return { amount: newItem.amount++, ...newItem };
           }
+          return newItem;
         });
-        setCartItems(tempArr);
-        return;
-      }
-      setCartItems([...cartItems, item]);
+      });
       return;
     }
-    setCartItems([...cartItems, item]);
-    return;
+    setCartItems((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: productId,
+          title: productTitle,
+          img: productImg,
+          price: productPrice,
+          amount: 1,
+        },
+      ];
+    });
   }
 
   return (
@@ -49,6 +52,8 @@ const ProductList = ({
           return (
             <Product
               key={product.id}
+              productPrice={product.price}
+              productCurrency={product.currency}
               productImg={product.img}
               productTitle={product.title}
               productId={product.id}
