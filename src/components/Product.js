0ad4551f450ffc3ebe1cb5311b2e82a78d04e8a2
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-06-23 23:17:47 +0500

    Show product price with currency

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,6 @@
 const Product = ({
+  productPrice,
+  productCurrency,
   productImg,
   productTitle,
   productId,
@@ -9,11 +11,18 @@ const Product = ({
       <div className="product-card-img">
         <img className="img" src={productImg} alt={productTitle} />
       </div>
+      <div className="product-price">
+        <p>
+          Price: {productPrice} {productCurrency}
+        </p>
+      </div>
       <button
         className="add-to-cart-btn"
         type="button"
         data-productid={productId}
-        onClick={() => onClickAddToCartBtn(productId, productImg, productTitle)}
+        onClick={() =>
+          onClickAddToCartBtn(productId, productImg, productTitle, productPrice)
+        }
       >
         Add to Card
       </button>
